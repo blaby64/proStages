93219a33273b03a0042ac0536db02edf4bb778e9
Benjamin Laby <blaby@iutbayonne.univ-pau.fr>
2019-11-12 17:01:14 +0100

    Codage de l'affichage

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -16,4 +16,36 @@ class ProStageController extends AbstractController
             'controller_name' => 'ProStageController',
         ]);
     }
+
+    /**
+     * @Route("/", name="pro_stage_accueil")
+     */
+    public function afficheBienvenue()
+    {
+        return new Response('<html><body><h1>Bienvenue sur la page d accueil de Prostages</h1></body></html>');
+    }
+
+    /**
+     * @Route("/entreprises", name="pro_stage_entreprises")
+     */
+    public function afficheEntreprises()
+    {
+        return new Response('<html><body><h1>Cette page affichera la liste des entreprises proposant un stage</h1></body></html>');
+    }
+
+    /**
+     * @Route("/formations", name="pro_stage_formations")
+     */
+    public function afficheFormations()
+    {
+        return new Response('<html><body><h1>Cette page affichera la liste des formations de l IUT</h1></body></html>');
+    }
+
+    /**
+     * @Route("/stages/{id}", name="pro_stage_id")
+     */
+    public function afficheId()
+    {
+        return new Response('<html><body><h1>Cette page affichera le descriptif du stage ayant pour identifiant id</h1></body></html>');
+    }
 }
