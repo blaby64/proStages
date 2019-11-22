91200a0ae964d938cc7a8cae1dc7d78160891bf7
Benjamin Laby <blaby@iutbayonne.univ-pau.fr>
2019-11-22 14:44:26 +0100

    Mise en place des routes

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -8,9 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ProStageController extends AbstractController
 {
-    /**
-     * @Route("/pro/stage", name="pro_stage")
-     */
+
     public function index()
     {
         return $this->render('pro_stage/index.html.twig', [
@@ -18,33 +16,22 @@ class ProStageController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/", name="pro_stage_accueil")
-     */
+
     public function afficheBienvenue()
     {
         return new Response('<html><body><h1>Bienvenue sur la page d accueil de Prostages</h1></body></html>');
     }
 
-    /**
-     * @Route("/entreprises", name="pro_stage_entreprises")
-     */
     public function afficheEntreprises()
     {
         return new Response('<html><body><h1>Cette page affichera la liste des entreprises proposant un stage</h1></body></html>');
     }
 
-    /**
-     * @Route("/formations", name="pro_stage_formations")
-     */
     public function afficheFormations()
     {
         return new Response('<html><body><h1>Cette page affichera la liste des formations de l IUT</h1></body></html>');
     }
 
-    /**
-     * @Route("/stages/{id}", name="pro_stage_id")
-     */
     public function afficheId($id)
     {
         return new Response('<html><body><h1>Cette page affichera le descriptif du stage ayant pour identifiant '.$id.'</h1></body></html>');
