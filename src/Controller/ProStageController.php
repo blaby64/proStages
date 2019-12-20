6008a2e1e00a133720c9c517c6166392d918401f
Benjamin Laby <blaby@iutbayonne.univ-pau.fr>
2019-12-20 11:47:22 +0100

    Création des données de test et début des pages stages par formation etc

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Stage;
+use App\Entity\Entreprise;
+use App\Entity\Formation;
+
 
 class ProStageController extends AbstractController
 {
@@ -19,21 +23,47 @@ class ProStageController extends AbstractController
 
     public function afficheBienvenue()
     {
-        return $this->render('pro_stage/accueil.html.twig');
+        $repositoryStage = $this->getDoctrine()->getRepository(Stage::class);
+        $stages=$repositoryStage->findall();
+        return $this->render('pro_stage/accueil.html.twig',['stages'=>$stages]);
     }
 
     public function afficheEntreprises()
     {
-        return $this->render('pro_stage/entreprises.html.twig');
+      $repositoryEntreprise = $this->getDoctrine()->getRepository(Entreprise::class);
+      $entreprises=$repositoryEntreprise->findall();
+      return $this->render('pro_stage/toute_les_entreprises.html.twig',['entreprises'=>$entreprises]);
     }
 
     public function afficheFormations()
     {
-        return $this->render('pro_stage/formations.html.twig');
+      $repositoryFormation = $this->getDoctrine()->getRepository(Formation::class);
+      $formations=$repositoryFormation->findall();
+      return $this->render('pro_stage/toute_les_formations.html.twig',['formations'=>$formations]);
     }
 
     public function afficheId($id)
     {
-        return $this->render('pro_stage/stagesid.html.twig',['idRessource'=>$id]);
+      $repoStage = $this->getDoctrine()->getRepository(Stage::class);
+      $stage = $repoStage->find($id);
+      return $this->render('pro_stage/stagesid.html.twig',
+      ['stage'=>$stage]);
+    }
+
+
+    public function entreprises($id)
+    {
+        $repoStage = $this->getDoctrine()->getRepository(Stage::class);
+        $stage = $repoStage->findByEntreprise($id);
+        return $this->render('pro_stage/entreprises.html.twig',
+        ['stages'=>$stage]);
+    }
+
+    public function formations($id)
+    {
+        $repoFormation = $this->getDoctrine()->getRepository(Formation::class);
+        $formation = $repoFormation->find($id);
+        return $this->render('pro_stage/formations.html.twig',
+        ['formation'=>$formation]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,92 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Formation;
+use App\Entity\Entreprise;
+use App\Entity\Stage;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = \Faker\Factory::create('fr_FR'); // Créer un faker français
+        //Création des formations
+        $formationDUTInfo = new Formation();
+        $formationLPProg = new Formation();
+        $formationDUTIC = new Formation();
+        $formationDUTInfo->setNom("DUT Informatique");
+        $formationDUTInfo->setNomLong("Diplome universitaire de technologie informatique");
+        $formationLPProg->setNom("LP Prog avancée");
+        $formationLPProg->setNomLong("Licence professionnel programmation avancée");
+        $formationDUTIC->setNom("DUT TIC");
+        $formationDUTIC->setNomLong("Diplome universitaire des technologies de l'information et de la communication");
+        $tabFormations = array($formationDUTInfo,$formationDUTIC,$formationLPProg);
+        $manager->persist($formationDUTInfo);
+        $manager->persist($formationDUTIC);
+        $manager->persist($formationLPProg);
 
+        //Création des entreprises
+        $tabEntreprises = array();
+        for($i = 0; $i < 5 ; $i++)
+        {
+            $entreprise = new Entreprise();
+            $nomEntr=$faker->company();
+            $nomSansSpeciaux = str_replace('.','',$nomEntr);
+            $nomSansEspaces = str_replace(' ','',$nomSansSpeciaux);
+            $entreprise->setNom($nomEntr);
+            $entreprise->setAdresse($faker->address());
+            $entreprise->setActivite($faker->jobtitle());
+            $entreprise->setSite($nomSansEspaces.$faker->regexify('\.(fr|es|com)'));
+            array_push($tabEntreprises,$entreprise);
+            $manager->persist($entreprise);
+        }
+        //Création des stages
+        foreach($tabEntreprises as $stageEntr)
+        {
+            for ($i=0; $i < 3; $i++)
+            {
+                $stage = new Stage();
+                $nbFormations = $faker->numberBetween(1,3); // Génération d'un nombre aléatoire pour définir le nombre de formations concernées apr le stage
+                switch($nbFormations)
+                {
+                    case 1:
+                    $choixFormation = $faker->numberBetween(0,2); // Génération d'un nombre aléatoire pour définir la formation qui concernera le stage
+                    $stage->addFormation($tabFormations[$choixFormation]);
+                    $tabFormations[$choixFormation]->addStage($stage);
+                    $manager->persist($tabFormations[$choixFormation]);
+                    break;
+                    case 2:
+                    $choixFormation1 = $faker->numberBetween(0,2); // Génération de deux nombres aléatoire pour définir les formations qui concerneront le stage
+                    $choixFormation2 = $faker->numberBetween(0,2);
+                    while($choixFormation1 == $choixFormation2) // Si les nombres générés sont identiques, en regénère un jusqu'à ce qu'il soit différent
+                    {
+                        $choixFormation2 = $faker->numberBetween(0,2);
+                    }
+                    $stage->addFormation($tabFormations[$choixFormation1]);
+                    $tabFormations[$choixFormation1]->addStage($stage);
+                    $stage->addFormation($tabFormations[$choixFormation2]);
+                    $tabFormations[$choixFormation2]->addStage($stage);
+                    $manager->persist($tabFormations[$choixFormation1]);
+                    $manager->persist($tabFormations[$choixFormation2]);
+                    break;
+                    case 3:
+                    foreach($tabFormations as $uneFormation)
+                    {
+                        $stage->addFormation($uneFormation);
+                        $uneFormation->addStage($stage);
+                        $manager->persist($uneFormation);
+                    }
+                    break;
+                }
+                $stage->setTitre($faker->jobtitle());
+                $stage->setEmail("contact".$faker->firstname()."@".$stageEntr->getsite());
+                $stage->setDescription($faker->realtext(255));
+                $stage->setEntreprise($stageEntr);
+                $stageEntr->addStage($stage);
+                $manager->persist($stageEntr);
+                $manager->persist($stage);
+            }
+        }
         $manager->flush();
     }
 }
