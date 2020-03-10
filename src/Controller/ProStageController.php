a1fe1044bd1b4502feff8941396cc1e64ca05070
Benjamin Laby <blaby@iutbayonne.univ-pau.fr>
2020-03-10 16:47:18 +0100

    Création et externalisation du formulaire d'ajout d'un stage

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -11,11 +11,10 @@ use App\Entity\Formation;
 use App\Repository\StageRepository;
 use App\Repository\EntrepriseRepository;
 use App\Repository\FormationRepository;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Form\EntrepriseType;
+use App\Form\StageType;
 
 class ProStageController extends AbstractController
 {
@@ -61,6 +60,24 @@ class ProStageController extends AbstractController
         }
 
         return $this->render('pro_stage/modifierEntreprise.html.twig', ['vueFormulaireEntreprise' => $formulaireEntreprise->createView()]);
+    }
+
+    public function ajouterStage(Request $requetteHttp, ObjectManager $manager)
+    {
+        $stage = new Stage();
+
+        $formulaireStage = $this -> createForm(StageType::class, $stage);
+
+        $formulaireStage->handleRequest($requetteHttp);
+
+        if($formulaireStage->isSubmitted() && $formulaireStage->isValid())
+        {
+            $manager->persist($stage);
+            $manager->flush();
+
+            return $this->redirectToRoute('proStage_accueil');
+        }
+        return $this->render('pro_stage/ajoutStage.html.twig', ['vueFormulaire' => $formulaireStage->createView()]);
     }
 
     public function afficheBienvenue(StageRepository $repoStage)
---
+++ src/Form/StageType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Stage;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use App\Form\EntrepriseType;
+use App\Entity\Formation;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class StageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre', TextType::class)
+            ->add('description', EmailType::class)
+            ->add('email', TextType::class)
+            ->add('entreprise', EntrepriseType::class)
+            ->add('formations', EntityType::class, ['class'=>Formation::class, 'choice_label' => 'nom', 'expanded' => true, 'multiple' => true])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Stage::class,
+        ]);
+    }
+}
