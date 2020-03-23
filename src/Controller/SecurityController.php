0db77a1641e00654f14dcda7a9854b09df615f91
Benjamin Laby <blaby@iutbayonne.univ-pau.fr>
2020-03-23 23:27:52 +0100

    Création du formulaire d'inscription

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
@@ -27,5 +32,31 @@ class SecurityController extends AbstractController
     public function logout()
     {
 
+    }
+
+    public function inscription(Request $requetteHttp, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
+    {
+        $user = new User();
+
+        $formulaireUser = $this->createForm(UserType::class, $user);
+
+        $formulaireUser->handleRequest($requetteHttp);
+
+        if($formulaireUser->isSubmitted())
+        {
+            //Ajout des roles
+            $user->setRoles(['ROLE_USER']);
+
+            //Encodage du mdp
+            $encodageMdp = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($encodageMdp);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/inscription.html.twig', ['vueFormulaireUser' => $formulaireUser->createView()]);
     }
 }
