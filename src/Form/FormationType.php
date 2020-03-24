c252da6ff64bfa976c2a3cd3cc0ea0e41dc6d067
Benjamin Laby <blaby@iutbayonne.univ-pau.fr>
2020-03-24 01:08:37 +0100

    Mise en place de l'administration des formations avec le CRUD et harmonisation des vues

---
+++ src/Form/FormationType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Formation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Stage;
+
+class FormationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('nomLong')
+            ->add('stages', EntityType::class, ['class' => Stage::class,
+                                                'choice_label' => 'titre',
+                                                'expanded' => true,
+                                                'multiple' => true]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Formation::class,
+        ]);
+    }
+}
